41abc0b7f0b804464a556e1c0a9b348265669c06
woly1011 <woly1.sarr@gmail.com>
2025-12-13 00:31:23 +0100

    Update schema.sql

--- projets/base_rh_sql/schema.sql
+++ projets/base_rh_sql/schema.sql
@@ -1,1 +1,14 @@
+CREATE TABLE employes (
+    id INT PRIMARY KEY,
+    nom VARCHAR(100),
+    poste VARCHAR(100),
+    salaire INT,
+    anciennete INT
+);
+
+INSERT INTO employes VALUES
+(1, 'Awa', 'Analyste', 25000, 2),
+(2, 'Moussa', 'Manager', 42000, 6),
+(3, 'Fatou', 'Data Analyst', 31000, 3),
+(4, 'Ibrahima', 'Stagiaire', 12000, 1);
 
